04931dd09ca372e94c64ccc5936c3b004f4bfebe
Gani Aliguzhinov <aliguzhinov.gani@gmail.com>
2024-04-02 18:40:33 +0200

    Added astrocommunity https://github.com/AstroNvim/astrocommunity config, changed theme

---
+++ lua/plugins/astrocommunity.lua
@@ -0,0 +1,61 @@
+-- Add name as folder name inside
+-- https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/colorscheme
+-- To enable, set colorscheme = "<colorscheme-name>" inside "astroui.lua"
+-- where <colorscheme-name> is same as in
+-- "colorscheme <colorscheme-name>" in the description
+-- of the theme on the theme's github repo
+
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim",  enabled = false },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim",  enabled = false },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.bamboo-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin",     enabled = true },
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  { import = "astrocommunity.color.transparent-nvim" },
+}
